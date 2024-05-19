6f14c5a0416933084331e61917f2a35c603f60d5
GakmanAlexey <gakman@ya.ru>
2024-05-19 14:23:42 +0300

    добавлен конфиг на хеад

--- mod/pages/controller/config.php
+++ mod/pages/controller/config.php
@@ -7,4 +7,24 @@ Class Config{
     public $pre_title   = "Пре";
     public $title       = "Тайтл по умолчанию";
     public $post_title  = "Пост";
+
+    public $pre_description   = "Пре";
+    public $description       = "Тайтл по умолчанию";
+    public $post_description  = "Пост";
+
+    public $pre_keys    = "Пре";
+    public $keys        = "Тайтл по умолчанию";
+    public $post_keys   = "Пост";
+
+    public $formanico = "";
+    public $ico = "";
+    public $generator = "";
+    public $themeColor = "";
+
+    public $ss_list = [];
+
+    public function __construct($h){
+
+        return $h;
+    }
 }
