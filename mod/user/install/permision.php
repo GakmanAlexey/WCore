1a7111ead7eb5b1b14dc58a48e7247f335a71566
GakmanAlexey <gakman@ya.ru>
2024-06-01 13:08:35 +0300

    save

---
+++ mod/user/install/permision.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Mod\User\Install;
+
+Class Permision extends \Mod\Abstract\Install{
+
+    public function install_BD($h){
+        return $h;
+    }
+
+    public function install_Router($h){
+
+        $array = [
+            "url"           => "/admin/users/permison/",
+            "class"         => "Mod\User\Controller\Admin\Permison",
+            "function"      => "index",
+            "title"         => "Группы",
+            "description"   => "Группы",
+            "keys"          => "Группы",
+            "name"          => "Группы",
+            "add_to_sitemap"=> true,
+            "lastmod_s"     => time(),
+            "change_s"      => "monthly",
+            "priority_s"    => 0.5
+        ];
+        $h["install"]["line"][] = $array;
+
+        $array = [
+            "url"           => "/admin/users/group/add/",
+            "class"         => "Mod\User\Controller\Admin\Permisonadd",
+            "function"      => "index",
+            "title"         => "Добавление группы",
+            "description"   => "Добавление группы",
+            "keys"          => "Добавление группы",
+            "name"          => "Добавление группы",
+            "add_to_sitemap"=> true,
+            "lastmod_s"     => time(),
+            "change_s"      => "monthly",
+            "priority_s"    => 0.5
+        ];
+        $h["install"]["line"][] = $array;
+
+        $array = [
+            "url"           => "/admin/users/group/edit/",
+            "class"         => "Mod\User\Controller\Admin\Permisondit",
+            "function"      => "index",
+            "title"         => "Редактирование группы",
+            "description"   => "Редактирование группы",
+            "keys"          => "Редактирование группы",
+            "name"          => "Редактирование группы",
+            "add_to_sitemap"=> true,
+            "lastmod_s"     => time(),
+            "change_s"      => "monthly",
+            "priority_s"    => 0.5
+        ];
+        $h["install"]["line"][] = $array;
+
+        $array = [
+            "url"           => "/admin/users/group/delet/",
+            "class"         => "Mod\User\Controller\Admin\Permisondelet",
+            "function"      => "index",
+            "title"         => "Удаление группы",
+            "description"   => "Удаление группы",
+            "keys"          => "Удаление группы",
+            "name"          => "Удаление группы",
+            "add_to_sitemap"=> true,
+            "lastmod_s"     => time(),
+            "change_s"      => "monthly",
+            "priority_s"    => 0.5
+        ];
+        $h["install"]["line"][] = $array;
+
+        return $h;
+    }
+
+
+    public function install_Congif($h){
+
+        return $h;
+    }
+    
+}
---
+++ mod/user/view/admin/permison.php
@@ -0,0 +1,79 @@
+
+<div class="main_crm">
+            <div class="main_crm_parent">
+
+                <div class="bread">
+                    <a class="bread_element">Группы</a>
+                </div>
+
+                <div class="main_crm_content margin_20px_top ">
+                    <div class="wrap_contant margin_10_30 linear_spase_between">
+                    <h3 class="main_crm_content_h3">Группы</h3>
+                        <a class="icon_button blue" href="/admin/users/group/add/">
+                            <img src="/src/admin/img/add-square.svg" alt="">
+                            Добавить группу
+                        </a>
+                    </div>
+                </div>
+
+
+
+                
+                    <div class="stroka_info">
+                        <div class="stroka_info_title">
+                                <div class="stroka_redactor_uprav"></div>
+                                <div class="stroka_redactor_id stroka_text_title">ID</div>
+                                <div class="stroka_redactor_name stroka_text_title stroka_text_elem_2in">Название</div>
+                                <div class="stroka_redactor_dopoln stroka_text_title stroka_text_elem_2in">Префикс</div>
+                                <div class="redact_3">
+                                </div>
+                        </div>
+                        <div class="stroka_element_no_scroll">
+
+    <div class="stroka_element">
+                                <div class="stroka_redactor_uprav">
+                                    <label class="parent_chek" for="checkbox">
+                                        <input class="checkbox" type="checkbox" name="checkbox">
+                                    </label>
+                                    <a class="img_link" href=""><img src="/src/admin/img/circ_three.svg" alt=""></a>
+                                </div>      
+                                <div class="stroka_redactor_name stroka_text_elem stroka_text_elem_2in">'.$item["name_ru"].'</div>
+                                <div class="stroka_redactor_dopoln stroka_text_elem stroka_text_elem_2in">'.$item["prefix"].'</div>
+                            </div>
+    
+    
+   
+                            
+                                                     
+                        </div>                                                    
+                    </div>
+
+                    
+                <div class="main_crm_content">
+                    <div class="linear_spase_between">
+                        <div class="wrap_element_flex">
+                            
+
+                            <p class="element_text_value">Всего страниц: <?php echo $h["pagin"]["page_count"];?></p>
+                        </div>
+
+                        
+                        <div class="wrap_element_flex">
+                            <div class="pagination">
+                                <?php
+                                foreach($h["pagin"]["page_line"] as $element){
+                                    if($element == "l") {
+                                        echo '<a href="?page=1" class="pagination_element"><img src="/src/admin/img/pagi_left.svg" alt=""></a>';
+                                    }elseif($element == "r") {
+                                        echo '<a href="?page='.$h["pagin"]["page_count"].'" class="pagination_element"><img src="/src/admin/img/pagi_right.svg" alt=""></a>';
+                                    }else{
+                                        echo '<a href="?page='.$element.'" class="pagination_element">'.$element.'</a>';
+                                    }                                    
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
