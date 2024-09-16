a73fab48e517214638f0ebb0d46152507ef2b177
GakmanAlexey <gakman@ya.ru>
2024-09-16 13:28:57 +0300

    добавлены 401 и 404

--- mod/pages/view/e401.php
+++ mod/pages/view/e401.php
@@ -1,1 +1,13 @@
-its e401
+
+<div class="container">
+        <h1 class="title_block text_al_center error_title">
+            401
+        </h1>
+        <div class="text_error text_al_center col_6 margin_0_auto">
+            Ошибка, что-то пошло не так. Возможно вы ошиблись, набирая адрес в сроке браузера.<br>
+            Если вы уверены, что адрес набран верно, значит страница была удалена и её больше нет.
+        </div>
+
+        <a class="ct_btn aa_ct_btn aa_color_btn col_3 margin_0_auto" href="/">На главную</a>
+
+    </div>
